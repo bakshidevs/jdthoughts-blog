8a5b7c08ceb70230423afd68a2ff1bca0b34f96c
bakshidevs <sunandasamantaofficial@gmail.com>
2025-07-21 01:23:00 +0530

    UI components

---
+++ src/components/ui/Tags.jsx
@@ -0,0 +1,7 @@
+export default function Tags({ text, className = '' }) {
+    return (
+        <span className={`bg-gradient-to-r from-cyan-500 via-blue-500 to-pink-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800 text-gray-100 text-xs font-medium me-2 px-2.5 py-1 rounded-full ${className}`}>
+            {text}
+        </span>
+    )
+}
